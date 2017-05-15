a429a43272326af508a9d734a6fdbc7fa8edbd50
David Llanes <david962406@gmail.com>
2017-05-14 20:07:28 -0500

    wfr

--- app/src/main/java/com/fiuady/android/domotics/AccessActivity.java
+++ app/src/main/java/com/fiuady/android/domotics/AccessActivity.java
@@ -3,7 +3,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AccessActivity extends AppCompatActivity {
-
+//fgjfgj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
