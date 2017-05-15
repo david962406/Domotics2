affbb46b333d8b521ca3ac22d33eec90af61fd9c
David Castillo <davidjosecastillodorbecker@hotmail.com>
2017-05-14 19:05:44 -0500

    rttr

--- app/src/main/java/com/fiuady/android/domotics/LoginActivity.java
+++ app/src/main/java/com/fiuady/android/domotics/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Holaa
+        //Holaar
         inventory=new Inventory (getApplicationContext());
         InventoryHelper.backupDatabaseFile(getApplicationContext());
 
