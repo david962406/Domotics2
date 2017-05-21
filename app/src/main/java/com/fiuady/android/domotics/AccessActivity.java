6e7272bbd69ebd9c2a8f62eb45915c7c88ba20be
David Llanes <david962406@gmail.com>
2017-05-20 20:45:14 -0500

    led junto con sensor temperatura fallos

--- app/src/main/java/com/fiuady/android/domotics/AccessActivity.java
+++ app/src/main/java/com/fiuady/android/domotics/AccessActivity.java
@@ -23,7 +23,7 @@ import java.io.InputStreamReader;
 import java.util.UUID;
 
 public class AccessActivity extends FragmentActivity {
-
+//
     Button btnNewUser;
     Button btnLed;
     Button btnDataSensor;
